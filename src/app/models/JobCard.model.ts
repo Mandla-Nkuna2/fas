9e5de3adaf857a9cb84ad379f44abb694293e7bb
unknown <algorsnipes@gmail.com>
2021-03-26 12:14:04 +0200

    added

---
+++ src/app/models/JobCard.model.ts
@@ -0,0 +1,5 @@
+export class JobCard    {
+    name: string;
+    surname: string;
+    reportedDate: string;
+}
--- src/app/pages/jobcard/jobcard.page.ts
+++ src/app/pages/jobcard/jobcard.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {JobCard} from '../../models/JobCard.model';
 @Component({
   selector: 'app-jobcard',
   templateUrl: './jobcard.page.html',
   styleUrls: ['./jobcard.page.scss'],
 })
 export class JobcardPage implements OnInit {
-
-  constructor() { }
+  jobCard: JobCard;
+  constructor() { 
+    this.jobCard = new JobCard();
+  }
 
   ngOnInit() {
+    
+    console.log()
+  }
+  onchange()  {
+    console.log("date")
+    console.log(this.jobCard.reportedDate);
   }
 
 }
