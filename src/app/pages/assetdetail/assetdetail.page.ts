c587f126733b4f44c0ebcb550c796d71302ba05b
algorsnipes@gmail.com <algorsnipes@gmail.com>
2021-04-08 10:28:42 +0200

    added test service

--- src/app/pages/assetdetail/assetdetail.page.ts
+++ src/app/pages/assetdetail/assetdetail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FirebaseService } from '../../services/firebase-service/firebase-service.service';
+import {PopupHelper} from '../../services/helpers/popup-helper';
 @Component({
   selector: 'app-assetdetail',
   templateUrl: './assetdetail.page.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssetdetailPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private firebaseSevice: FirebaseService,
+    private popUp: PopupHelper
+  ) { }
 
   ngOnInit() {
+    let assets =  {
+      name: 'awdwdawdawd',
+      surname: 'adawdawdawdawd',
+      date: 'adawdawdaw',
+      uncle: 'adawawdwa'
+    }
+    this.firebaseSevice.saveAssets(assets, '1234').then((s) => {
+      this.popUp.showAlert('Success', 'Your data has been saved :)')
+    }).catch((errror) =>  {
+      this.popUp.showError(errror);
+    })
   }
 
 }
--- src/app/services/firebase-service/firebase-service.service.ts
+++ src/app/services/firebase-service/firebase-service.service.ts
@@ -1,14 +1,32 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { AlertController, LoadingController, ToastController } from '@ionic/angular'; 
 import {  AngularFirestore } from '@angular/fire/firestore';
-
+import {Asset} from '../../models/Asset.model';
+import {LoadingService} from '../../services/loading-service/loading.service';
 @Injectable({
     providedIn: 'root',
 })
 export class FirebaseService { 
 constructor(
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private loading: LoadingService
 )   {
 
 }
+    public saveAssets(assets: any, id: string) {
+        const promise = new Promise((resolve, reject) => {
+            this.loading.present('loading...').then(() =>   {
+                this.afs.collection('test4/Sup_OilGrade/tables').doc(id).set(assets).then(() => {
+                    this.loading.dismiss()
+                    resolve('done')
+                }).catch((error) => {
+                    this.loading.dismiss();
+                    reject(error)
+                })
+            })
+
+        })
+        return promise;
+    }
+
 }
